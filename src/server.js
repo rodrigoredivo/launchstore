c4284019be83ce0de32e5ad5d1525fe352a16b85
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-07-28 21:42:25 -0300

    Global variables with express and nunjucks

--- src/server.js
+++ src/server.js
@@ -7,6 +7,11 @@ const server = express()
 
 // MIDDLEWARES
 server.use(session)
+server.use((req, res, next) => {
+  res.locals.session = req.session
+  next()
+})
+
 server.use(express.urlencoded({extended: true}))
 server.use(express.static('public'))
 server.use(methodOverride('_method'))
