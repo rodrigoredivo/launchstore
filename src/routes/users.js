c6bef355fcbae58bd680af06e2ec4b863549a4d2
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-07-29 22:29:34 -0300

    Password recovery request form

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,7 @@ const UserController = require("../app/controller/UserController")
 const UserValidator = require("../app/validators/user")
 const SessionValidator = require("../app/validators/session")
 
-const { isLoggedRedirectToUsers } = require("../app/middlewares/session")
+const { isLoggedRedirectToUsers, onlyUsers } = require("../app/middlewares/session")
 
 // LOGIN/LOGOUT
 routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
@@ -14,16 +14,16 @@ routes.post('/login',SessionValidator.login , SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // RESET PASSWORD / FORGOT
-//routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/forgot-password', SessionController.forgotForm)
 //routes.get('/forgout-reset', SessionController.resetForm)
-//routes.post('/forgot-password', SessionController.forgot)
+routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
 //routes.post('/password-reset', SessionController.reset)
 
 // USER REGISTER UserController
 routes.get('/register', UserController.registerForm)
 routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', UserValidator.show, UserController.show)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
 routes.put('/',UserValidator.update, UserController.update)
 //routes.delete('/', UserController.delete)
 
