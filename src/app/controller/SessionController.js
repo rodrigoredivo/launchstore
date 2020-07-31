509ecd3e6da55ea0ff91b71a9d52cf1e58cd63b1
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-07-30 23:49:28 -0300

    Encrypted key for password recovery request

--- src/app/controller/SessionController.js
+++ src/app/controller/SessionController.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto")
+
 module.exports = {
   loginForm(req,res) {
     return res.render("session/login")
@@ -15,6 +17,19 @@ module.exports = {
     return res.render("session/forgot-password")
   },
   forgot(req, res) {
-    
+    const user = req.user
+
+    // token para o usuário
+    const token = crypto.randomBytes(20).toString("hex")
+
+    // Criar uma expiração do token
+    let now = new Date()
+    now = now.setHours(now.getHours() + 1)
+
+    await user.update(user.id, {
+      reset_token: token,
+      reset_token_expires: now
+    })
+
   }
 }
